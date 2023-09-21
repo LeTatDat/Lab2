652c8454a21ee3af9825ab51acc4d555c95e75d2
LeTatDat <105649726+LeTatDat@users.noreply.github.com>
2023-09-21 07:29:01 +0700

    Add files via upload

---
+++ Models/MenuItems.cs
@@ -0,0 +1,9 @@
+﻿namespace TH1.Models
+{
+    public class MenuItems
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using TH1.Models;
+
+namespace TH1.ViewComponents
+{
+    public class RenderViewComponent:ViewComponent
+    {
+        private List<MenuItems> MenuItems = new List<MenuItems>();
+        public RenderViewComponent()
+        {
+            MenuItems = new List<MenuItems>()
+            {
+                new MenuItems() {Id=1, Name="Branches", Link="Branches/List"},
+                new MenuItems() { Id = 2, Name = "Students", Link = "Students/List" },
+                new MenuItems() { Id = 3, Name = "Subjects", Link = "Subjects/List" },
+                new MenuItems() { Id = 4, Name = "Courses", Link = "Courses/List" },
+            };
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderLeftMenu", MenuItems);
+        }
+    }
+}
+
